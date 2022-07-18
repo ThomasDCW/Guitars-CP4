6094c77cb4e9f43833b350415b7fdbad07cadf7f
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-18 15:39:39 +0200

    init the app on the frontend

---
+++ frontend/src/GlobalStyle.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export default styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+  font-family: "Poppins", sans-serif;
+  background-color: tomato;
+`;
