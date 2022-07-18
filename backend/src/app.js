1f5df7354f6ebec88826b55c6acb9b1b11ab6311
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-18 17:11:10 +0200

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,18 @@ const cors = require("cors");
 const router = require("./router");
 
 const app = express();
-
+const corsWhitelist = [process.env.FRONTEND_URL, process.env.ADMIN_URL];
 // use some application-level middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (corsWhitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
