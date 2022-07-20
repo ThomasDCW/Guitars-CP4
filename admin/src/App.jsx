1eec0f643d7b3aa815296a8e0aa04cedb75cc33d
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-20 10:54:20 +0200

    can create, edit, delete data with react admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,19 @@
 import { Admin, Resource } from "react-admin";
 import GuitarList from "@components/GuitarList";
 import GuitarEdit from "@components/GuitarEdit";
+import GuitarCreate from "@components/GuitarCreate";
 import dataProvider from "./dataProvider";
 
 function App() {
   return (
     <div className="App">
       <Admin dataProvider={dataProvider}>
-        <Resource name="guitars" list={GuitarList} edit={GuitarEdit} />
+        <Resource
+          name="guitars"
+          list={GuitarList}
+          edit={GuitarEdit}
+          create={GuitarCreate}
+        />
       </Admin>
     </div>
   );
