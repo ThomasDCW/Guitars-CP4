7e454fcb85c185f352585c1151138efe2a9f4adc
ThomasDCW <thomas.dicola@hotmail.fr>
2022-07-21 10:45:38 +0200

    update readme and update database.sql

--- README.md
+++ README.md
@@ -1,34 +1,15 @@
-## Concept
+Checkpoint 4 wild code school
 
-This template is meant to serve as a foundation for every P2/P3 following the React-Express-MySQL stack, as learned in Wild Code School.
-It's pre-configured with a set of tools which'll help students produce industry-quality and easier-to-maintain code, while staying as simple as possible to use.
+Product backlog : https://docs.google.com/spreadsheets/d/161h6Pik1XXHdpatj3u1abj3wlGGlGWki2pEt9ZhlSpI/edit#gid=0
+Figma : https://www.figma.com/file/Trs9yuFUv4IhOrhYeBqswI/CP4?node-id=0%3A1
 
-## Setup & Use
+BDD : 1 table guitar qui contient les colonnes suivantes
 
-### Project Initialization
+id
+picture
+label
+brand
+price
+style
 
-- In VSCode, install plugins **Prettier - Code formatter** and **ESLint** and configure them
-- Clone this repo, enter it
-- Run command `npm run setup`
-- _NB: To launch the backend server, you'll need an environment file with database credentials. You'll find a template one in `backend/.env.sample`_
-
-### Available Commands
-
-- `setup` : Initialization of frontend and backend, as well as all toolings
-- `dev` : Starts both servers (frontend + backend) in one terminal
-- `dev-front` : Starts the React frontend server
-- `dev-back` : Starts the Express backend server
-- `lint` : Runs validation tools, and refuses unclean code (will be executed on every _commit_)
-- `fix` : Fixes linter errors (run it if `lint` growls on your code !)
-
-## FAQ
-
-### Tools
-
-- _Concurrently_ : Allows for several commands to run concurrently in the same CLI
-- _Husky_ : Allows to execute specific commands that trigger on _git_ events
-- _Vite_ : Alternative to _Create-React-App_, packaging less tools for a more fluid experience
-- _ESLint_ : "Quality of code" tool, ensures chosen rules will be enforced
-- _Prettier_ : "Quality of code" tool as well, focuses on the styleguide
-- _ Airbnb Standard_ : One of the most known "standards", even though it's not officially linked to ES/JS
-- _Nodemon_ : Allows to restart the server everytime a .js file is udated
+Migrate.js dans la console pour récuperer la bdd en local
--- backend/database.sql
+++ backend/database.sql
@@ -1,63 +1,55 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: guitares.com
+-- ------------------------------------------------------
+-- Server version	8.0.29-0ubuntu0.20.04.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `simple-mvc`
+-- Table structure for table `guitar`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `guitar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `guitar` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `picture` varchar(512) NOT NULL,
+  `label` varchar(32) NOT NULL,
+  `brand` varchar(64) NOT NULL,
+  `price` int NOT NULL,
+  `style` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `item`
+-- Dumping data for table `guitar`
 --
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+LOCK TABLES `guitar` WRITE;
+/*!40000 ALTER TABLE `guitar` DISABLE KEYS */;
+INSERT INTO `guitar` VALUES (1,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/334572.webp','Santos Series C-40SCE BK','Harley Benton',169,'Classic'),(2,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/459355.webp','Classic 4/4 Guitar','Thomann',120,'Classic'),(3,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/462511.webp','Les Paul Standard 60s UB','Gibson',2290,'Electric'),(5,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/544035.webp','Affinity Strat HSS SVB FSR','Squier',269,'Electric'),(6,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/521102.webp','Fusion-III HSH EB FBLB','Harley Benton',298,'Electric'),(7,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/515179.webp','ML1 Modern Rainstorm Blue','Chapmans Guitar',759,'Electric'),(8,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/536886.webp','Exile X Floyd Black Satin','Dean',473,'Electric'),(9,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/405978.webp','Guthrie Govan HSH Cooked Ash','Charvel',3698,'Electric'),(10,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/533083.webp','Hernandez 2-Ovangkol','Alberto',364,'Classic'),(11,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/494055.webp','Kodaira AST-100','Asturias',1529,'Classic'),(12,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/457725.webp','CC-60SCE Nat WN','Fender',259,'Folk'),(13,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/462334.webp','Roadhouse Faded Black A/E','Art & Lutherie',819,'Folk'),(14,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/460211.webp','L-00 Deluxe Rosewood Burst','Gibson',3290,'Folk'),(15,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/483250.webp','GS Mini-e Koa Plus','Taylor',1099,'Folk'),(16,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/444422.webp','GN51CE-NAT','Takamine',459,'Folk'),(17,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/459703.webp','Kanilea AS-4 Soprano','Islander',126,'Ukulele'),(18,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/457322.webp','CV 70s Jazz Bass MN BK','Squier',399,'Electric Bass'),(19,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/437403.webp','CB-60SCE A-Bass Black 2018','Fender',266,'Acoustic Bass'),(20,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/400266.webp','Baby Taylor BTe Koa','Taylor',649,'Travel Guitar'),(21,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/545681.webp','G9500 Ltd Edition Jim Dandy','Gretch',199,'Folk'),(22,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/483932.webp','LTD F-204 BLKS','ESP',579,'Electric Bass'),(23,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/419217.webp','Thunderbird 60´s Bass TS','Epiphone',645,'Electric Bass'),(24,'https://thumbs.static-thomann.de/thumb/thumb220x220/pics/prod/340705.webp','TB-70VS Deluxe Series','Harley Benton',198,'Electric Bass');
+/*!40000 ALTER TABLE `guitar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-21 10:40:00
